5b798a8df5195833c9a311b9168e4cf11fb38b59
msavita-cloud <msavita19@gmail.com>
2024-02-12 10:54:26 -0800

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/Contoso-ff70ad88.svg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
